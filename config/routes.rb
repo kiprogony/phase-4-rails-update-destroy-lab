1637129ffea6d59d373437bf23074965f31a9a93
Daryl Rogony <darylrogony@gmail.com>
2022-07-20 11:31:21 +0300

    Fit: Fetch requests routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants
+# endapplication.routes.draw do
+#   resources :plants, only: [:index, :show, :create]
+# end
+# endapplication.routes.draw do
+#   resources :plants, only: [:index, :show, :create]
 end
